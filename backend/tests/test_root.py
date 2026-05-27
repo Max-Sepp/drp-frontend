2c4323054ce184846ac579a41207876e64940f5b
Constantin Filip <dinu.filip.self@gmail.com>
2026-05-27 10:51:34 +0100

    [cf924] fix(backend): Update changed test

--- backend/tests/test_root.py
+++ backend/tests/test_root.py
@@ -4,4 +4,4 @@ from fastapi.testclient import TestClient
 def test_read_root(client: TestClient) -> None:
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"Hello": "There is nothing here yet, but you can check out the /docs for API documentation!"}
