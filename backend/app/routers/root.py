0e260ead638872097c754ef76cfb67b243e854b3
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2026-05-27 19:36:32 +0100

    feat(backend): Remove item model, router, and related tests to streamline equipment issues reporting

--- backend/app/routers/root.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(tags=["root"])
-
-
-@router.get("/")
-def read_root() -> dict[str, str]:
-    return {"Hello": "There is nothing here yet, but you can check out the /docs for API documentation!"}
